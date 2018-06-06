1dad7184e1fd17c18a494c17c689c4ef1dd1c303
陶伟基Wiki <taoweiji2008@qq.com>
2018-06-06 09:44:35 +0800

    Update Database.java
    SOURCE -> RUNTIME

--- dbflow-core/src/main/java/com/raizlabs/android/dbflow/annotation/Database.java
+++ dbflow-core/src/main/java/com/raizlabs/android/dbflow/annotation/Database.java
@@ -17,7 +17,7 @@ import java.lang.annotation.Target;
  * </p>
  */
 @Target(ElementType.TYPE)
-@Retention(RetentionPolicy.SOURCE)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface Database {
 
     /**
